5b42aed58c9bfb5db9dc55b66510a4c39209aaec
Ayan Hussain <codewithayanhn@gmail.com>
2025-10-22 18:54:09 +0530

    creation or insertion in bst

---
+++ lec111/pg1.cpp
@@ -0,0 +1,42 @@
+//Inserttion or creation of Binary Search Tree
+#include<iostream>
+using namespace std;
+
+//creating Node class 
+class Node
+{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int value)
+    {
+        int data=value;
+        left=right=NULL;
+    }
+};
+
+Node* insert(Node* root,int target) //here we can do pass by reference to save some space 
+{
+    if(!root)
+    {
+        Node* temp = new Node(target);
+        return temp;
+    }
+
+    if(target<root->data)
+    root->left = insert(root->left,target);
+    else root->right = insert(root->right,target);
+
+    return root; //root ke ander hamesha root ka adress hi jarha hoga
+}
+int main()
+{
+    int arr[]={3,7,4,1,6,8};
+    Node* root=NULL;
+    //inserting element one by one from array using recursive function
+    for(int i=0;i<6;i++)
+    root=insert(root,arr[i]); 
+    // we create here our tree but how we know that it get created successfully, for that we have to know traversal (pg2.cpp)
+
+}
